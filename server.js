cc8a3bcb06b5f8bfb120247ce08ab5a214b4d369
Jake Zook <jakezook01@gmail.com>
2023-09-26 15:53:03 -0600

    Changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const api = require('./routes/api');
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
